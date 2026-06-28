a16f3b63e43b786ad878860c10bd1a196a1aa5c2
Muslimaxon <dilbarxongulomova85@gmail.com>
2026-06-28 21:56:56 +0500

    WhiteNoise va static storage sozlandi

--- core/settings.py
+++ core/settings.py
@@ -118,5 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'crm_main/static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
