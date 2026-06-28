047f74a7e3bb9f59e3983da0bd12d2af2c07a6ca
Muslimaxon <dilbarxongulomova85@gmail.com>
2026-06-28 19:48:41 +0500

    Asosiy sahifa uchun url qo'shildi

--- crm_main/urls.py
+++ crm_main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import student_list_view, group_list_view
 
 urlpatterns = [
+    path('', student_list_view, name='home'), # Asosiy manzilga kirganda student_list_view ochiladi
     path('students/', student_list_view, name='student_list'),
-    path('groups/', group_list_view, name='group_list'), # Yangi manzil
+    path('groups/', group_list_view, name='group_list'),
 ]
